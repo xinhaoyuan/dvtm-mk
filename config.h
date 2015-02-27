5fb9fbb66b6e49f3f1f1ccdcf03dce52f824b36c
xy2189 <xinhaoyuan@gmail.com>
2015-02-27 14:54:45 -0500

    .

--- config.h
+++ config.h
@@ -33,7 +33,7 @@ static Color colors[] = {
 /* status bar (command line option -s) position */
 #define BAR_POS         BAR_TOP /* BAR_BOTTOM, BAR_OFF */
 /* whether status bar should be hidden if only one client exists */
-#define BAR_AUTOHIDE    true
+#define BAR_AUTOHIDE    false
 /* master width factor [0.1 .. 0.9] */
 #define MFACT 0.5
 /* number of clients in master area */
@@ -66,7 +66,7 @@ static Layout layouts[] = {
 	{ "[ ]", fullscreen },
 };
 
-#define MOD "A-`"
+#define MOD "A-z"
 #define NR_BINDING_MODE 2
 
 /* you can at most specifiy MAX_ARGS (3) number of arguments */
